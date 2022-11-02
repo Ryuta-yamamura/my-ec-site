62435d3a8ee48dc8105a874054180ee353558557
ryuta-yamamura <longtaishancun554@gmail.com>
2022-11-02 13:59:28 +0900

    変更

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,11 +1,19 @@
 // components/layout.js
-
+import Seo from "./Seo"
 import Navbar from './Navbar'
 import Footer from './Footer'
 
 export default function Layout({ children }) {
   return (
     <>
+      <Seo
+        pageTitle={'official website'}
+        pageDescription={'ページの概要文を記載します。'}
+        pageImg={'https://demo.com'}
+        pageImgWidth={1280}
+        pageImgHeight={960}
+        fontPath = {'https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap'}
+      />
       <Navbar />
       <main>{children}</main>
       <Footer />
--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -1,28 +1,48 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import css from '../css/navbar.module.css'
+import css from '../css/navbar.module.scss'
+import React, { useState } from "react"
 
 export default function App() {
-    return (
-        <header className="header">
-        <div className={css.headerInner}>
-          <Link href="/">
-            <Image className={css.headerLogo} src="/homelogo.jpg" alt="logo" width={90} height={70}></Image>
-          </Link>
-          <button className={css.toggleMenuButton}></button>
-          <div className="header-site-menu">
-            <nav className={css.siteMenu}>
-              <ul>
-              {/* <li><Link href="/CONCEPT"><a>CONCEPT</a></Link></li> */}
-              <li><a href="/CONCEPT">CONCEPT</a></li>
+
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuFunction = () => {
+    console.log('openMenu is' && openMenu)
+    setOpenMenu(!openMenu);
+    console.log('openMenu is' && openMenu)
+    console.log('clicked!')
+  }
+  return (
+    <header className={css.header}>
+      <div className={css.headerInner}>
+        <Link href="/">
+          <Image className={css.headerLogo} src="/homelogo.jpg" alt="logo" width={90} height={70}></Image>
+        </Link>
+        {/* <button className={css.toggleMenuButton}></button> */}
+        <div className={css.toggleMenuButton} onClick={() => menuFunction()}>
+          <span className={openMenu ? css.open : undefined}></span>
+          <span className={openMenu ? css.open : undefined}></span>
+          <p className={openMenu ? css.open : undefined}>Menu</p>
+        </div>
+        <div className={`${css.headerSiteMenu} ${openMenu ? css.open : undefined}`}>
+          <nav className={css.siteMenu}>
+
+            <ul>
+              <div className={css.close} onClick={() => menuFunction()}>
+                <span></span>
+                <span></span>
+                <p>Close</p>
+              </div>
+              <li><Link href="/CONCEPT">CONCEPT</Link></li>
               <li><Link href="/MENU">MENU</Link></li>
               <li><Link href="/SHOP">SHOP</Link></li>
               <li><Link href="/ACCESS">ACCESS</Link></li>
-              </ul>
-            </nav>
-          </div>
+            </ul>
+          </nav>
         </div>
-      </header>
 
-    )
+      </div>
+    </header>
+
+  )
 }  
